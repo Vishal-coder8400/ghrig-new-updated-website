a20848aeb3422177b48711b69024b00bf631f4c5
Vishal-coder8400 <vktgkp7@gmail.com>
2026-01-14 18:20:53 +0530

    changes from client

--- app/home/banner_section_new.jsx
+++ app/home/banner_section_new.jsx
@@ -3,30 +3,89 @@ import Link from "next/link";
 
 export default function BannerSection() {
   return (
-    <section className="w-full overflow-hidden">
-      <div className="hero-wrapper relative">
+    <>
+      {/* ================= DESKTOP SECTION ================= */}
+      <section className="w-full overflow-hidden">
+   <div className="hero-wrapper relative min-h-screen pt-[88px] hidden md:block">
 
-        {/* LEFT TOP CARD */}
-        <div className="absolute top-[22%] left-[4%] w-[15%] h-[32%]" />
 
-        {/* LEFT BOTTOM CARD */}
-        <div className="absolute top-[39%] left-[20%] w-[18%] h-[34%]" />
+        
+        {/* HEADING */}
+        {/*
+        <div className="flex justify-center">
+          <div className="flex justify-center items-center pt-[140px]">
+            <h1
+              className="text-white text-center font_family_jakarta 
+                         text-[68px] leading-[72px] font-semibold"
+            >
+              The Online Recruitment
+              <br /> Marketplace
+            </h1>
+          </div>
+        </div>
+        */}
 
-        {/* MIDDLE */}
-        <div className="absolute top-[39%] left-[42%] w-[18%] h-[34%]" />
+        {/* LEFT TOP CARD (TRAINERS) */}
+        <div
+          className="absolute top-[22%] left-[4%] w-[15%] h-[32%] cursor-pointer"
+          id="trainerCard"
+        />
 
-        {/* RIGHT BOTTOM */}
-        <div className="absolute top-[39%] left-[64%] w-[18%] h-[34%]" />
+        {/* LEFT BOTTOM CARD (JOB SEEKERS) */}
+        <div
+          className="absolute top-[39%] left-[20%] w-[18%] h-[34%] cursor-pointer"
+          id="jobCard"
+        />
 
-        {/* RIGHT TOP */}
-        <div className="absolute top-[22%] right-[4%] w-[15%] h-[32%]" />
+        {/* MIDDLE CARD (CORPORATE) */}
+        <div
+          className="absolute top-[39%] left-[42%] w-[18%] h-[34%] cursor-pointer"
+          id="corporateCard"
+        />
+
+        {/* RIGHT BOTTOM CARD (RECRUITERS) */}
+        <div
+          className="absolute top-[39%] left-[64%] w-[18%] h-[34%] cursor-pointer"
+          id="recruiterCard"
+        />
+
+        {/* RIGHT TOP CARD (REFERRERS) */}
+        <div
+          className="absolute top-[22%] right-[4%] w-[15%] h-[32%] cursor-pointer"
+          id="referrerCard"
+        />
 
         {/* CENTER LOGO */}
         <Link href="#">
-          <div className="absolute top-[29%] left-[45.5%] w-[9%] h-[9%]" />
+          <div
+            className="absolute top-[29%] left-[45.5%] w-[9%] h-[9%] cursor-pointer"
+            id="logoCard"
+          />
         </Link>
-
       </div>
-    </section>
+      </section>
+
+      {/* ================= MOBILE SECTION ================= */}
+      <div className="hero-wrapper-mobile relative block md:hidden">
+        {/* <div className="flex justify-center">
+          <div className="flex justify-center items-start pb-10 mt-10">
+            <h1
+              className="text-white text-center font_family_jakarta
+                         text-[42px] leading-[52px] font-semibold"
+            >
+              The Online Recruitment
+              <br /> Marketplace
+            </h1>
+          </div>
+        </div> */}
+
+        {/* MOBILE CARD POSITIONS */}
+        <div className="absolute w-[32%] top-[68%] left-[8%]" id="m1" />
+        <div className="absolute w-[32%] top-[68%] left-[36%]" id="m2" />
+        <div className="absolute w-[32%] top-[68%] left-[64%]" id="m3" />
+        <div className="absolute w-[32%] top-[36%] left-[50%]" id="m4" />
+        <div className="absolute w-[32%] top-[36%] left-[15%]" id="m5" />
+      </div>
+    </>
   );
 }
